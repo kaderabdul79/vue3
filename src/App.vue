93cd72a97cfb0384a9ace3c72d1c64b51a2bd928
kaderabdul <kaderabdul7980@gmail.com>
2022-06-26 19:46:33 +0600

    design the navbar

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavbarApp/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NavbarApp from './components/NavbarApp.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    NavbarApp:NavbarApp
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/NavbarApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="wrapper">
+    <div class="logo">A.Kader</div>
+    <div class="navbar">
+        <a href="">About</a>
+        <a href="">About</a>
+        <a href="">About</a>
+        <a href="">About</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'NavbarApp'
+}
+</script>
+
+<style>
+
+</style>
